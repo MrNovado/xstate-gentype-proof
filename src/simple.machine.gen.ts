2e0bd582eb2e065d0a4e67b35774ae6f8b6c1c40
MrNovado <novado@ymail.com>
2020-03-24 21:16:45 +0300

    fixing schema composition

--- src/simple.machine.gen.ts
+++ src/simple.machine.gen.ts
@@ -9,6 +9,7 @@ export const schema = (
   "states": {
     "1": {
       "id": "1",
+      "initial": "_1",
       "states": {
         "11": {
           "id": "11",
@@ -22,13 +23,14 @@ export const schema = (
           "id": "12",
           "entry": "onEntry",
           "on": {
-            "PREV": "12",
+            "PREV": "11",
             "NEXT": "13",
             "OUTER": "#1"
           }
         },
         "13": {
           "id": "13",
+          "initial": "_13",
           "states": {
             "131": {
               "id": "131",
@@ -45,27 +47,37 @@ export const schema = (
                 "PREV": "131",
                 "OUTER": "#13"
               }
+            },
+            "_13": {
+              "id": "_13",
+              "entry": "onEntry",
+              "on": {
+                "PREV": "#12",
+                "INNER": "131",
+                "OUTER": "#1"
+              }
             }
           },
+          "entry": "onEntry"
+        },
+        "_1": {
+          "id": "_1",
           "entry": "onEntry",
           "on": {
-            "PREV": "12",
-            "INNER": ".131",
-            "OUTER": "#1"
+            "NEXT": "#2",
+            "INNER": "11"
           }
         }
       },
-      "entry": "onEntry",
-      "on": {
-        "NEXT": "2",
-        "INNER": ".11"
-      }
+      "entry": "onEntry"
     },
     "2": {
       "id": "2",
+      "initial": "_2",
       "states": {
         "21": {
           "id": "21",
+          "initial": "_21",
           "states": {
             "211": {
               "id": "211",
@@ -73,20 +85,28 @@ export const schema = (
               "on": {
                 "OUTER": "#21"
               }
+            },
+            "_21": {
+              "id": "_21",
+              "entry": "onEntry",
+              "on": {
+                "OUTER": "#2",
+                "INNER": "211"
+              }
             }
           },
+          "entry": "onEntry"
+        },
+        "_2": {
+          "id": "_2",
           "entry": "onEntry",
           "on": {
-            "OUTER": "#2",
-            "INNER": ".211"
+            "PREV": "#1",
+            "INNER": "21"
           }
         }
       },
-      "entry": "onEntry",
-      "on": {
-        "PREV": "1",
-        "INNER": ".21"
-      }
+      "entry": "onEntry"
     }
   }
 }
@@ -107,41 +127,39 @@ export type Paths =
     | ["1,13"]
     | ["1,13,131"]
     | ["1,13,132"]
+    | ["1,13,_13"]
+    | ["1,_1"]
     | ["2"]
     | ["2,21"]
     | ["2,21,211"]
+    | ["2,21,_21"]
+    | ["2,_2"]
 ;
 
 export type EventType = 
     | "NEXT"
-    | "INNER"
     | "OUTER"
     | "PREV"
+    | "INNER"
 ;
 
 export enum EventEnum {
   NEXT = "NEXT",
-  INNER = "INNER",
   OUTER = "OUTER",
-  PREV = "PREV"
+  PREV = "PREV",
+  INNER = "INNER"
 };
 
 export const eventList: EventType[] = [
   "NEXT",
-  "INNER",
   "OUTER",
-  "PREV"
+  "PREV",
+  "INNER"
 ];
 
 export const state2EventMap = {
-  "1": [
-    "NEXT",
-    "INNER"
-  ],
-  "2": [
-    "PREV",
-    "INNER"
-  ],
+  "1": [],
+  "2": [],
   "1,11": [
     "NEXT",
     "OUTER"
@@ -151,11 +169,7 @@ export const state2EventMap = {
     "NEXT",
     "OUTER"
   ],
-  "1,13": [
-    "PREV",
-    "INNER",
-    "OUTER"
-  ],
+  "1,13": [],
   "1,13,131": [
     "NEXT",
     "OUTER"
@@ -164,12 +178,26 @@ export const state2EventMap = {
     "PREV",
     "OUTER"
   ],
-  "2,21": [
+  "1,13,_13": [
+    "PREV",
+    "INNER",
+    "OUTER"
+  ],
+  "1,_1": [
+    "NEXT",
+    "INNER"
+  ],
+  "2,21": [],
+  "2,21,211": [
+    "OUTER"
+  ],
+  "2,21,_21": [
     "OUTER",
     "INNER"
   ],
-  "2,21,211": [
-    "OUTER"
+  "2,_2": [
+    "PREV",
+    "INNER"
   ]
 }
 
